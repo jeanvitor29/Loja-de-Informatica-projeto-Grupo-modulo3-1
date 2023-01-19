1e3ddb4ae48054c9133e07f6bfd9328070cadd1b
ismael <ismaeldj1234567@gmail.com>
2023-01-19 08:02:09 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./routes/Home"
+import Navibar from "./component/Navibar"
+import Footer from "./component/Footer"
+
+function App() {
+  return (
+    <div className="App">
+    <Navibar />
+    <Home />
+    <Footer />
+    </div>
+  )
+}
+
+export default App
