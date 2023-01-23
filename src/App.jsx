11ffb687a4c6793f33602a9b9da93c22473877de
ismael <ismaeldj1234567@gmail.com>
2023-01-23 07:53:19 -0300

    navibar e footer com icons linkdos para o github

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Navbar from "./component/NavBar/Navbar"
 import {Outlet} from 'react-router-dom'
+import Footer from "./component/Footer/Footer"
 function App() {
   return (
     <div className="App">
     <Navbar />
-    <div className="container">
-        <Outlet />
-      </div>
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
     </div>
   )
 }
